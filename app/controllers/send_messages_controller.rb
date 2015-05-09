c136305119905090713276cef73d26b7149319bd
Margo Urey <margo@margonline.co.uk>
2015-05-09 11:45:15 +0100

    update send method

--- app/controllers/send_messages_controller.rb
+++ app/controllers/send_messages_controller.rb
@@ -4,20 +4,20 @@ class SendMessagesController < ApplicationController
     @message = Message.new
   end
 
-  # def send
-  #   number_to_send_to = params[:phone]
-  #   twilio_sid = 'ACaf6af8863d70c2c6aa4c77579e7bced6'
-  #   twilio_token ='c5251c8b4e84b6e0ae478cb4712bd391'
-  #   twilio_Phone_number = '903683040'
+  def send
+    number_to_send_to = params[:phone]
+    twilio_sid = ENV['TWILIO_SID']
+    twilio_token = ENV['TWILIO_TOKEN']
+    twilio_Phone_number = ENV['TWILIO_PHONE_NUMBER']
 
-  #   @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
-  #   @twilio_client.account.sms.messages.create(
-  #     :from => "+1#{twilio_phone_number}",
-  #     :to => number_to_send_to,
-  #     :body => "Twilio message to be sent to #{number_to_send_to}"
-  #     )
+    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+    @twilio_client.account.sms.messages.create(
+      :from => "+1#{twilio_phone_number}",
+      :to => number_to_send_to,
+      :body => "Twilio message to be sent to #{number_to_send_to}"
+      )
 
-  #   flash.notice = 'Your message has been sent'
-  #   redirect_to '/'
-  # end
+    flash.notice = 'Your message has been sent'
+    redirect_to '/'
+  end
 end
