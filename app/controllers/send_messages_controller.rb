d83b443021e889af7ed8e346b97d2b737d3448c6
Margo Urey <margo@margonline.co.uk>
2015-05-09 11:45:15 +0100

    update send method

--- app/controllers/send_messages_controller.rb
+++ app/controllers/send_messages_controller.rb
@@ -6,9 +6,9 @@ class SendMessagesController < ApplicationController
 
   # def send
   #   number_to_send_to = params[:phone]
-  #   twilio_sid = 'ACaf6af8863d70c2c6aa4c77579e7bced6'
-  #   twilio_token ='c5251c8b4e84b6e0ae478cb4712bd391'
-  #   twilio_Phone_number = '903683040'
+  #   twilio_sid = ENV[TWILIO_SID]
+  #   twilio_token ='ENV[TWILIO_TOKEN]
+  #   twilio_Phone_number = ENV[TWILIO_PHONE_NUMBER]
 
   #   @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
   #   @twilio_client.account.sms.messages.create(
